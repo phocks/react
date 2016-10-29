6b8e26eb3d321094f3cd013af0d63afdc26ddcc6
Joshua Byrd <phocks@gmail.com>
2016-10-29 12:45:57 +1000

    Hello world. i get this kinda

--- js/main.jsx
+++ js/main.jsx
@@ -1,4 +1,8 @@
-const element = <h1>Hello, world</h1>;
+// Our main React file here or something
+
+// Define a react element or something
+const element = <h1>Hello world.</h1>;
+
 ReactDOM.render(
   element,
   document.getElementById('root')
